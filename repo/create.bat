1bf4dc29f57c84db5eaf29242285f22fd9255511
Florian Lagg <kontakt@florian.lagg.at>
2016-08-29 22:55:48 +0200

    * try another generator for repository * upload generated files

--- repo/create.bat
+++
@@ -1,9 +0,0 @@
-REM # Edited Chad Parry's script to run on windwos:
-REM # os.path.samefile not available on windows
-REM def samefile(path1, path2):
-REM     return os.path.normcase(os.path.normpath(path1)) == os.path.normcase(os.path.normpath(path2))
-
-h:
-cd H:\dev\LaggAt\LaggAt-Kodi\repo
-python H:\dev\3rdParty\kodi-repo-script.chad.parry.org\tools\create_repository.py ..\src\repository.LaggAt ..\src\script.audioswitch
-pause
